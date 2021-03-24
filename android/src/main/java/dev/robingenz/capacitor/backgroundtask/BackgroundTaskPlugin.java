5314f436386b1acbbe18786b5b082953f022458b
Robin Genz <mail@robingenz.dev>
2021-03-24 20:08:56 +0100

    feat: implement android (#2)

--- android/src/main/java/dev/robingenz/capacitor/backgroundtask/BackgroundTaskPlugin.java
+++ android/src/main/java/dev/robingenz/capacitor/backgroundtask/BackgroundTaskPlugin.java
@@ -11,12 +11,19 @@ public class BackgroundTaskPlugin extends Plugin {
 
     private BackgroundTask implementation = new BackgroundTask();
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
+    @PluginMethod(returnType=PluginMethod.RETURN_CALLBACK)
+    public void beforeExit(PluginCall call) {
+        // No-op for now. Android support will be added in a later version.
+        String callbackId = call.getCallbackId();
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("taskId", callbackId);
         call.resolve(ret);
     }
+
+    @PluginMethod()
+    public void finish(PluginCall call) {
+        // No-op for now. Android support will be added in a later version.
+        call.resolve();
+    }
+
 }
