2278abcc217277b31e9759b7555e4a7d2cb354d6
Robin Genz <mail@robingenz.dev>
2021-03-24 21:38:41 +0100

    ci: add gh workflow

--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { BackgroundTaskPlugin, CallbackID, FinishOptions } from './definitions';
 
 export class BackgroundTaskWeb extends WebPlugin implements BackgroundTaskPlugin {
+  // eslint-disable-next-line @typescript-eslint/ban-types
   public beforeExit(_cb: Function): CallbackID {
     throw this.unimplemented('Not implemented on web.');
   }
