b66b4a2eefb53606bf157a0252e9f6ae76ddeb45
Robin Genz <mail@robingenz.dev>
2021-03-24 21:39:29 +0100

    style: format

--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,14 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { BackgroundTaskPlugin, CallbackID, FinishOptions } from './definitions';
+import type {
+  BackgroundTaskPlugin,
+  CallbackID,
+  FinishOptions,
+} from './definitions';
 
-export class BackgroundTaskWeb extends WebPlugin implements BackgroundTaskPlugin {
+export class BackgroundTaskWeb
+  extends WebPlugin
+  implements BackgroundTaskPlugin {
   // eslint-disable-next-line @typescript-eslint/ban-types
   public beforeExit(_cb: Function): CallbackID {
     throw this.unimplemented('Not implemented on web.');
