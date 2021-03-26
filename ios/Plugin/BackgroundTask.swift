05c808d9169de7516fe28e2302110350f215f8fc
Robin Genz <mail@robingenz.dev>
2021-03-27 00:24:44 +0100

    refactor(ios): perform the task on a background queue (#9)

--- ios/Plugin/BackgroundTask.swift
+++ ios/Plugin/BackgroundTask.swift
@@ -5,19 +5,24 @@ import UIKit
     var taskIds: [String: UIBackgroundTaskIdentifier] = [:]
 
     @objc public func beforeExit(_ callbackId: String) {
-        var taskId = UIBackgroundTaskIdentifier.invalid
-        taskId = UIApplication.shared.beginBackgroundTask {
-            // Finish the task if time expires.
-            self.finish(callbackId)
+        DispatchQueue.global().async {
+            var taskId = UIBackgroundTaskIdentifier.invalid
+            taskId = UIApplication.shared.beginBackgroundTask {
+                // Finish the task if time expires.
+                UIApplication.shared.endBackgroundTask(taskId)
+                self.taskIds.removeValue(forKey: callbackId)
+            }
+            self.taskIds[callbackId] = taskId
         }
-        self.taskIds[callbackId] = taskId
     }
 
     @objc public func finish(_ callbackId: String) {
-        guard let taskId = self.taskIds[callbackId] else {
-            return
+        DispatchQueue.global().async {
+            guard let taskId = self.taskIds[callbackId] else {
+                return
+            }
+            UIApplication.shared.endBackgroundTask(taskId)
+            self.taskIds.removeValue(forKey: callbackId)
         }
-        self.taskIds.removeValue(forKey: callbackId)
-        UIApplication.shared.endBackgroundTask(taskId)
     }
 }
